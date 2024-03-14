e678981826efd2ff23d7c35e335e52966842b297
AmitabhNinave <55440005+AmitabhNinave@users.noreply.github.com>
2024-03-14 17:37:00 +0530

    Create Book.cs

---
+++ Book.cs
@@ -0,0 +1,8 @@
+public class Book
+{
+  public Guid Id { get; set; }
+  public string? Title { get; set; }
+  public string? Author { get; set; }
+  public string? Gender { get; set; }
+  public DateTime ReleaseDate { get; set; }
+}
